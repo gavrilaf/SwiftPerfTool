f4021cfe743494e5bda2272ca8c5445bc7f947ed
eugenf <eugenf78@gmail.com>
2018-08-20 07:19:42 +0300

    add missed latency test

--- Tests/SwiftPerfToolTests/LatencyMeasureTests.swift
+++ Tests/SwiftPerfToolTests/LatencyMeasureTests.swift
@@ -26,6 +26,20 @@ final class LatencyMeasureTests: XCTestCase {
         XCTAssertTrue(50...52 ~= res.scores / 1000)
     }
     
+    func testLatencyMultipleRun2() {
+        let cfg = SFTConfig(iterations: 1, trials: [ { Thread.sleep(forTimeInterval: 0.1) },
+                                                     { Thread.sleep(forTimeInterval: 0.1) },
+                                                     { Thread.sleep(forTimeInterval: 0.1) } ])
+        
+        let res = runMeasure(with: cfg)
+        
+        XCTAssertTrue(res.total ==~ 3*res.mean)
+        XCTAssertTrue(res.sd.isApproximateEqual(to: 0, epsilon: 0.01))
+        
+        XCTAssertTrue(res.total.isApproximateEqual(to: 0.3, epsilon: 0.01))
+        XCTAssertTrue(50...52 ~= res.scores / 1000)
+    }
+    
     func testScores() {
         let r1 = runMeasure(with: SFTConfig(iterations: 5, trials: [ { Thread.sleep(forTimeInterval: 0.1) } ]))
         let r2 = runMeasure(with: SFTConfig(iterations: 5, trials: [ { Thread.sleep(forTimeInterval: 0.05) } ]))
@@ -41,6 +55,7 @@ final class LatencyMeasureTests: XCTestCase {
     static var allTests = [
         ("testLatencySingleRun", testLatencySingleRun),
         ("testLatencyMultipleRun", testLatencyMultipleRun),
+        ("testLatencyMultipleRun2", testLatencyMultipleRun2),
         ("testScores", testScores),
     ]
 }
